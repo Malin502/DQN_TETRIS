931e17887d6ebdf92019a1dba844e952364e5c5a
Takuto Maekawa <148175105+Malin502@users.noreply.github.com>
2024-06-25 16:47:57 +0900

    大幅に構成を変更

---
+++ Agent.py
@@ -0,0 +1,17 @@
+import torch.nn as nn
+
+class DeepQNetwork(nn.Module):
+    def __init__(self):
+        super(DeepQNetwork, self).__init__()
+
+        self.conv1 = nn.Sequential(nn.Linear(4, 64), nn.ReLU(inplace=True))
+        self.conv2 = nn.Sequential(nn.Linear(64, 64), nn.ReLU(inplace=True))
+        self.conv3 = nn.Sequential(nn.Linear(64, 1))
+
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+
+        return x
